1c4e1f440870c6aa2eaab7d5fe4530db9fb58269
neerajp99 <neerajp1999@gmail.com>
2019-11-14 16:43:12 +0530

    Update keys

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://neeraj:neeraj123@ds033579.mlab.com:33579/e-travel"
+  mongoURI: "mongodb://neeraj:neeraj123@ds033579.mlab.com:33579/e-travel",
+  secretOrKey: 'generativeaiisthefuture'
 };

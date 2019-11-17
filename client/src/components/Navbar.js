9ccc892e7b76eeaf87f77fe0b8ea874831ae7a24
neerajp99 <neerajp1999@gmail.com>
2019-11-17 15:52:48 +0530

    Add Navbar

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="navbar navbar-expand-lg navbar-dark standard_navbar">
+        <a className="navbar-brand standard_heading" href="#">
+          {" "}
+          Some Random Shit
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarTogglerDemo01"
+          aria-controls="navbarTogglerDemo01"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <form className="form-inline my-2 my-lg-0 standard_form">
+            <button className="btn btn-outline-success my-2 my-sm-0 standard_register_button">
+              Register
+            </button>
+            <button className="btn btn-outline-success my-2 my-sm-0 standard_login_button">
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Navbar;

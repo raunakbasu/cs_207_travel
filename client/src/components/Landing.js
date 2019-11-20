c6d6f9efc85dea38dc1802033033f9453635d3c3
neerajp99 <neerajp1999@gmail.com>
2019-11-20 15:34:03 +0530

    Edit Landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Landing extends Component {
   render() {
@@ -6,43 +7,138 @@ class Landing extends Component {
       <div className="landing">
         <div className="landing_first">Hello world</div>
         <div className="landing_search">
-          <ul class="nav nav-tabs" role="tablist">
-            <li class="nav-item">
-              <a
-                class="nav-link active"
-                href="#profile"
-                role="tab"
-                data-toggle="tab"
+          <div class="col-xs-12 ">
+            <nav>
+              <div
+                className="nav nav-tabs nav-fill"
+                id="nav-tab"
+                role="tablist"
               >
-                profile
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#buzz" role="tab" data-toggle="tab">
-                buzz
-              </a>
-            </li>
-            <li class="nav-item">
-              <a
-                class="nav-link"
-                href="#references"
-                role="tab"
-                data-toggle="tab"
+                <a
+                  className="nav-item nav-link active"
+                  id="nav-home-tab"
+                  data-toggle="tab"
+                  href="#nav-home"
+                  role="tab"
+                  aria-controls="nav-home"
+                  aria-selected="true"
+                >
+                  Flights
+                </a>
+                <a
+                  className="nav-item nav-link"
+                  id="nav-profile-tab"
+                  data-toggle="tab"
+                  href="#nav-profile"
+                  role="tab"
+                  aria-controls="nav-profile"
+                  aria-selected="false"
+                >
+                  Trains
+                </a>
+                <a
+                  className="nav-item nav-link"
+                  id="nav-contact-tab"
+                  data-toggle="tab"
+                  href="#nav-contact"
+                  role="tab"
+                  aria-controls="nav-contact"
+                  aria-selected="false"
+                >
+                  Hotels
+                </a>
+              </div>
+            </nav>
+            <div className="tab-content py-3 px-3 px-sm-0" id="nav-tabContent">
+              <div
+                className="tab-pane fade show active"
+                id="nav-home"
+                role="tabpanel"
+                aria-labelledby="nav-home-tab"
               >
-                references
-              </a>
-            </li>
-          </ul>
-
-          <div class="tab-content">
-            <div role="tabpanel" class="tab-pane fade in active" id="profile">
-              ...
-            </div>
-            <div role="tabpanel" class="tab-pane fade" id="buzz">
-              bbb
-            </div>
-            <div role="tabpanel" class="tab-pane fade" id="references">
-              ccc
+                <ul className="bullshit_one">
+                  <li className="bullshit_one_1">
+                    <input
+                      className="form-control form-control-lg text-field"
+                      placeholder="From"
+                      name="from"
+                      onChange={this.onChange}
+                    />
+                  </li>
+                  <li className="bullshit_one_1">
+                  <input
+                    className="form-control form-control-lg text-field"
+                    placeholder="To"
+                    name="to"
+                    onChange={this.onChange}
+                  />
+                  </li>
+                  <li className="bullshit_one_1">
+                  <input
+                    className="form-control form-control-lg text-field"
+                    placeholder="From"
+                    name="from"
+                    onChange={this.onChange}
+                  />
+                  </li>
+                  <li className="bullshit_one_1">
+                  <input
+                    className="form-control form-control-lg text-field"
+                    placeholder="From"
+                    name="from"
+                    onChange={this.onChange}
+                  />
+                  </li>
+                </ul>
+              </div>
+              <div
+                className="tab-pane fade"
+                id="nav-profile"
+                role="tabpanel"
+                aria-labelledby="nav-profile-tab"
+              >
+                Et et consectetur ipsum labore excepteur est proident excepteur
+                ad velit occaecat qui minim occaecat veniam. Fugiat veniam
+                incididunt anim aliqua enim pariatur veniam sunt est aute sit
+                dolor anim. Velit non irure adipisicing aliqua ullamco irure
+                incididunt irure non esse consectetur nostrud minim non minim
+                occaecat. Amet duis do nisi duis veniam non est eiusmod tempor
+                incididunt tempor dolor ipsum in qui sit. Exercitation mollit
+                sit culpa nisi culpa non adipisicing reprehenderit do dolore.
+                Duis reprehenderit occaecat anim ullamco ad duis occaecat ex.
+              </div>
+              <div
+                className="tab-pane fade"
+                id="nav-contact"
+                role="tabpanel"
+                aria-labelledby="nav-contact-tab"
+              >
+                Et et consectetur ipsum labore excepteur est proident excepteur
+                ad velit occaecat qui minim occaecat veniam. Fugiat veniam
+                incididunt anim aliqua enim pariatur veniam sunt est aute sit
+                dolor anim. Velit non irure adipisicing aliqua ullamco irure
+                incididunt irure non esse consectetur nostrud minim non minim
+                occaecat. Amet duis do nisi duis veniam non est eiusmod tempor
+                incididunt tempor dolor ipsum in qui sit. Exercitation mollit
+                sit culpa nisi culpa non adipisicing reprehenderit do dolore.
+                Duis reprehenderit occaecat anim ullamco ad duis occaecat ex.
+              </div>
+              <div
+                className="tab-pane fade"
+                id="nav-about"
+                role="tabpanel"
+                aria-labelledby="nav-about-tab"
+              >
+                Et et consectetur ipsum labore excepteur est proident excepteur
+                ad velit occaecat qui minim occaecat veniam. Fugiat veniam
+                incididunt anim aliqua enim pariatur veniam sunt est aute sit
+                dolor anim. Velit non irure adipisicing aliqua ullamco irure
+                incididunt irure non esse consectetur nostrud minim non minim
+                occaecat. Amet duis do nisi duis veniam non est eiusmod tempor
+                incididunt tempor dolor ipsum in qui sit. Exercitation mollit
+                sit culpa nisi culpa non adipisicing reprehenderit do dolore.
+                Duis reprehenderit occaecat anim ullamco ad duis occaecat ex.
+              </div>
             </div>
           </div>
         </div>

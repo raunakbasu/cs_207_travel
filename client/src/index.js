4e83ae2c69b658434691497b001ab5f025830e1f
neerajp99 <neerajp1999@gmail.com>
2019-11-26 21:22:31 +0530

    Modify root directory

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,8 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+// import "react-date-range/dist/styles.css"; // main style file
+// import "react-date-range/dist/theme/default.css"; // theme css file
 
 ReactDOM.render(
   <BrowserRouter>

2edec3771e2ee7810a9c638f12228f6149b27b84
neerajp99 <neerajp1999@gmail.com>
2019-11-28 20:45:57 +0530

    Modify app

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Landing from "./components/Landing";
 import Navbar from "./components/Navbar";
+import FlightList from "./components/FlightList";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Router>
         <Route exact path="/" render={() => <Navbar />} />
         <Route exact path="/" render={() => <Landing />} />
+        <Route exact path="/flightlist" render={() => <FlightList />} />
       </Router>
     </div>
   );

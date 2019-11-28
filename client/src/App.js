07e6c2c89ceb1e596ffad2ecd9ef497d92ac4727
neerajp99 <neerajp1999@gmail.com>
2019-11-29 05:02:37 +0530

    Edit app with hote; routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Landing from "./components/Landing";
 import Navbar from "./components/Navbar";
 import FlightList from "./components/FlightList";
+import HotelsList from "./components/HotelsList";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route exact path="/" render={() => <Navbar />} />
         <Route exact path="/" render={() => <Landing />} />
         <Route exact path="/flightlist" render={() => <FlightList />} />
+        <Route exact path="/hotellist" render={() => <HotelsList />} />
       </Router>
     </div>
   );

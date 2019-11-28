def0c417f3f7677a2bc7c073c3e4f46c6e2bc7b9
neerajp99 <neerajp1999@gmail.com>
2019-11-28 20:46:36 +0530

    Edit footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,73 +1,106 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-class Footer extends Component{
-  render(){
-    return(
-
-
-<footer class="new_footer_area bg_color">
-            <div class="new_footer_top">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-3 col-md-6">
-                            <div class="f_widget company_widget wow fadeInLeft" data-wow-delay="0.2s" >
-                                <h3 class="f-title f_600 t_color f_size_18">Get in Touch</h3>
-                                <p>Don’t miss any updates of our new templates and extensions.!</p>
-                                <form action="#" class="f_subscribe_two mailchimp" method="post" novalidate="true" _lpchecked="1">
-                                    <input type="text" name="EMAIL" class="form-control memail" placeholder="Email"/>
-                                    <button class="btn btn_get btn_get_two" type="submit">Subscribe</button>
-                                    <p class="mchimp-errmessage" ></p>
-                                    <p class="mchimp-sucmessage" ></p>
-                                </form>
-                            </div>
-                        </div>
-                        <div class="col-lg-3 col-md-6">
-                            <div class="f_widget about-widget pl_70 wow fadeInLeft" data-wow-delay="0.4s">
-                                <h3 class="f-title f_600 t_color f_size_18">Download</h3>
-                                <ul class="list-unstyled f_list">
-                                    <li><a href="#">Company</a></li>
-                                    <li><a href="#">Android App</a></li>
-                                    <li><a href="#">ios App</a></li>
-                                    <li><a href="#">Desktop</a></li>
-                                    <li><a href="#">Projects</a></li>
-                                    <li><a href="#">My tasks</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="col-lg-3 col-md-6">
-                            <div class="f_widget about-widget pl_70 wow fadeInLeft" data-wow-delay="0.6s" >
-                                <h3 class="f-title f_600 t_color f_size_18">Help</h3>
-                                <ul class="list-unstyled f_list">
-                                    <li><a href="#">FAQ</a></li>
-                                    <li><a href="#">Term &amp; conditions</a></li>
-                                    <li><a href="#">Reporting</a></li>
-                                    <li><a href="#">Documentation</a></li>
-                                    <li><a href="#">Support Policy</a></li>
-                                    <li><a href="#">Privacy</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="col-lg-3 col-md-6">
-                            <div class="f_widget social-widget pl_70 wow fadeInLeft" data-wow-delay="0.8s" >
-                                <h3 class="f-title f_600 t_color f_size_18">Team Solutions</h3>
-                                <div class="f_social_icon">
-                                    <a href="#" class="fab fa-facebook"></a>
-                                    <a href="#" class="fab fa-twitter"></a>
-                                    <a href="#" class="fab fa-linkedin"></a>
-                                    <a href="#" class="fab fa-pinterest"></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="new_footer_area bg_color">
+        <div className="new_footer_top">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-3 col-md-6">
+                <div
+                  className="f_widget company_widget wow fadeInLeft"
+                  data-wow-delay="0.2s"
+                >
+                  <h3 className="f-title f_600 t_color f_size_18">About </h3>
+                  <p>
+                     The world has become so fast paced that people don’t
+                    want to stand by reading a page of information, they would
+                    much rather look at a presentation and understand the
+                    message. It has come to a point where images and videos are
+                    used more to
+                  </p>
                 </div>
-                <div class="footer_bg">
-                    <div class="footer_bg_one"></div>
-                    <div class="footer_bg_two"></div>
+              </div>
+              <div className="col-lg-3 col-md-6">
+                <div
+                  className="f_widget about-widget pl_70 wow fadeInLeft"
+                  data-wow-delay="0.4s"
+                >
+                  <h3 className="f-title f_600 t_color f_size_18">Explore</h3>
+                  <ul className="list-unstyled f_list">
+                    <li>
+                      <a href="#">Company</a>
+                    </li>
+                    <li>
+                      <a href="#">Android App</a>
+                    </li>
+                    <li>
+                      <a href="#">ios App</a>
+                    </li>
+                    <li>
+                      <a href="#">Desktop</a>
+                    </li>
+                    <li>
+                      <a href="#">Projects</a>
+                    </li>
+                    <li>
+                      <a href="#">My tasks</a>
+                    </li>
+                  </ul>
                 </div>
+              </div>
+              <div className="col-lg-3 col-md-6">
+                <div
+                  className="f_widget about-widget pl_70 wow fadeInLeft"
+                  data-wow-delay="0.6s"
+                >
+                  <h3 className="f-title f_600 t_color f_size_18">Company</h3>
+                  <ul className="list-unstyled f_list">
+                    <li>
+                      <a href="#">FAQ</a>
+                    </li>
+                    <li>
+                      <a href="#">Term &amp; conditions</a>
+                    </li>
+                    <li>
+                      <a href="#">Reporting</a>
+                    </li>
+                    <li>
+                      <a href="#">Documentation</a>
+                    </li>
+                    <li>
+                      <a href="#">Support Policy</a>
+                    </li>
+                    <li>
+                      <a href="#">Privacy</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="col-lg-3 col-md-6">
+                <div
+                  className="f_widget social-widget pl_70 wow fadeInLeft"
+                  data-wow-delay="0.8s"
+                >
+                  <h3 className="f-title f_600 t_color f_size_18">Connect</h3>
+                  <div className="f_social_icon">
+                    <a href="#" className="fab fa-facebook" />
+                    <a href="#" className="fab fa-twitter" />
+                    <a href="#" className="fab fa-linkedin" />
+                    <a href="#" className="fab fa-pinterest" />
+                  </div>
+                </div>
+              </div>
             </div>
-
-        </footer>
-    )
+          </div>
+          <div className="footer_bg">
+            <div className="footer_bg_one" />
+            <div className="footer_bg_two" />
+          </div>
+        </div>
+      </footer>
+    );
   }
 }
-export default Footer
+export default Footer;
